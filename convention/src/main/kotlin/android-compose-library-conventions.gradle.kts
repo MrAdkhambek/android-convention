d421aa1cdb0947725da3672ccc6edaa91ed7292e
Adkhambek <mr.adkhambek@gmail.com>
2025-03-06 00:08:07 +0500

    add support for androidx compose

---
+++ convention/src/main/kotlin/android-compose-library-conventions.gradle.kts
@@ -0,0 +1,50 @@
+import extension.findLibraryOrThrow
+import extension.libraries
+import extension.versions
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-parcelize")
+}
+
+android {
+
+    compileSdk = libraries.versions.compileSdk
+
+    compileOptions {
+        sourceCompatibility = libraries.versions.sourceCompatibility
+        targetCompatibility = libraries.versions.targetCompatibility
+        isCoreLibraryDesugaringEnabled = true
+    }
+
+    defaultConfig {
+        minSdk = libraries.versions.minSdk
+        testOptions.targetSdk = libraries.versions.targetSdk // needed for instrumental tests
+    }
+
+    packaging {
+        resources {
+            excludes.add("META-INF/LICENSE.md")
+            excludes.add("META-INF/LICENSE-notice.md")
+//            excludes.add("META-INF/*.kotlin_module")
+        }
+    }
+
+    project.kotlin {
+        jvmToolchain(jdkVersion = libraries.versions.jdk.asInt())
+    }
+
+    kotlinOptions {
+        jvmTarget = libraries.versions.jdk.toString()
+        freeCompilerArgs += listOf(
+            "-Xjsr305=strict",
+            "-Xexplicit-api=warning",
+            "-Xcontext-receivers"
+        )
+    }
+
+    dependencies {
+        add("coreLibraryDesugaring", libraries.findLibraryOrThrow("coreLibraryDesugaring").get())
+    }
+}
